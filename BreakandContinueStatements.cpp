4c52f5b1ba66eaf5020ee92d836f3a9ea89f1c32
cbrown17 <cassandrabrown80@gmail.com>
2020-02-25 13:05:57 -0600

    Add files via upload

---
+++ BreakandContinueStatements.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main() {
+  
+  /* break statement 
+  int sum = 0;
+  cout << "Enter values for x" << endl;
+  double x;
+  for(int k=1; k <=3; ++k)
+  {
+    cin >> x; 
+    if(x > 10.0)
+      break;
+    sum += x; 
+
+  }
+  cout << "Sum = " << sum << endl; 
+
+  */
+
+  /* continue statement 
+  int sum = 0;
+  cout << "Enter values for x" << endl;
+  double x;
+  for(int k=1; k <=3; ++k)
+  {
+    cin >> x; 
+    if(x > 10.0) //the sum of all values enter will print if this                                                    condition is true 
+      continue; //otherwise if a value is greater than 10.0 the continue statement cause the control to skip the rest of the statements in that iteration of the loop and continue with the next iteration of the loop. 
+    sum += x; 
+
+  }
+  cout << "Sum = " << sum << endl; 
+ */ 
+
+}
